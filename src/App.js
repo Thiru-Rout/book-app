f3ad7ab2cbe9f7c7c4aa6519f5a6af05ef4a2bd2
Thiru3369 <89295206+Thiru-Rout@users.noreply.github.com>
2021-11-17 13:23:09 +0530

    feat: new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Header from "./header";
+import Main from "./main";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
